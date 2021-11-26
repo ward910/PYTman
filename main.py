c5e7ed15d11cf22288deb3eca38876ae0bb57f58
Vincius Barbosa de Aguiar <vb9763662@gmail.com>
2021-11-26 16:08:52 -0300

    mod main.py

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from flask import Flask 
+import datetime
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    nameArticle = 'Python'
+    date = datetime.date.today().strftime('%m/%d/%Y')
+    text = 'Python is a high-level programming language, interpreted as scripting, imperative, object-oriented, functional, dynamic and strong typing. (Wikipedia)'
+    linkNavbr = ''
+    return render_template('index.html', nameArticle=nameArticle, date=date, textArticle=text, linkNavbr=linkNavbr)
+
+
+
+if __name__ == "__main__":
+    app.run()

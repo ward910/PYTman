eaa34288de194563171723601de5f4087f1118af
Vincius Barbosa de Aguiar <vb9763662@gmail.com>
2021-11-28 19:11:52 -0300

    mod main.py

--- main.py
+++ main.py
@@ -12,7 +12,12 @@ def index():
     date = datetime.date.today().strftime('%m/%d/%Y')
     return render_template('index.html', titleArticle=titleArticle, date=date, textArticle=textArticle, linkArticle=linkArticle)
 
-
+@app.route('/about')
+def about():
+    textAbout = """My first site using Flask and hosted on heroku used bootstrap 4.1 (I didn't use 5 because only 4.1 documentation was translated to my language).
+                   I created a system that every time a user enters it shows a different subject but it didn't go as planned because heroku is static and it just seemed to change the subject when I do a rebuild.
+                   This site is a proof of knowledge for me and I found it very easy and thought it would be more difficult."""
+    return render_template('about.html', textAbout=textAbout)
 
 if __name__ == "__main__":
     app.run()

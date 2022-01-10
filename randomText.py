4568194e5d63f18a4a53b8effb2a8817421c39ff
Vincius Barbosa de Aguiar <vb9763662@gmail.com>
2022-01-10 18:30:29 -0300

    mod randomText.py

--- randomText.py
+++ randomText.py
@@ -4,14 +4,14 @@ def rText():
     titleRandom = ['Python', 'Django', 'Pandas']
     textRandom = ['Python is a high-level programming language, interpreted as scripting, imperative, object-oriented, functional, dynamic and strong typing. (Wikipedia)', 'Django is a Python-based free and open-source web framework that follows the model–template–views architectural pattern. (Wikipedia)', 'In computer programming, pandas is a software library written for the Python programming language for data manipulation and analysis. (Wikipedia)']
     linkRandom = ['https://en.wikipedia.org/wiki/Python', 'https://en.wikipedia.org/wiki/Django', 'https://en.wikipedia.org/wiki/Pandas_(software)']
-    random = randint(0, 2)
+    numberRandom = randint(0, 2)
 
-    rTextExports = [titleRandom, textRandom, linkRandom, random]
+    rTextExports = [titleRandom, textRandom, linkRandom, numberRandom]
     return rTextExports
 
 rTextExports = rText()
 
 exports = {
     "titleRandom": rTextExports[0], "textRandom": rTextExports[1],
-    "linkRandom": rTextExports[2], "random": rTextExports[3]
+    "linkRandom": rTextExports[2], "numberRandom": rTextExports[3]
 }
